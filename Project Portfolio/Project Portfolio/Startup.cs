453c02d15b3367c8a957af255eb00dce8b70f5b7
Bronze Brown <dhamon.nightstar@gmail.com>
2018-01-03 11:25:37 -0500

    Create Project

---
+++ Project Portfolio/Project Portfolio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Project_Portfolio.Startup))]
+namespace Project_Portfolio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
