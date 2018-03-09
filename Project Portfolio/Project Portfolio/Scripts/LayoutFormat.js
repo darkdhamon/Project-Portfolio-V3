d43189fe78ca8ad0d6754e52e7e0b10b9d228731
Bronze Brown <dhamon.nightstar@gmail.com>
2018-03-09 08:59:46 -0500

    Site Layout & UI Updates
     - Created Number Editor Template
     - Updated CSS ID's and Classes for Styles updates.
     - Replaced Header
     - Update Home page
     - Update Edit Project Page
     - Create Project Layout Page
     - Update Project cards
     - update Project List Page
     - update About Side Tab in about layout
     - Improve Layout JS for website
     - CSS updates

--- Project Portfolio/Project Portfolio/Scripts/LayoutFormat.js
+++ Project Portfolio/Project Portfolio/Scripts/LayoutFormat.js
@@ -1,9 +1,13 @@
 ﻿
     var firstload = true;
     function resize() {
-        var headerHeight = $("header.navbar").outerHeight();
+        var headerHeight = $("header.fixed-top").outerHeight();
+        var footerHeight = $("footer.fixed-bottom").outerHeight();
         $("div.body-content").css("margin-top", headerHeight);
+        $("div.side-nav").css("margin-top", headerHeight);
+        $("div.body-content").css("margin-bottom", footerHeight);
         var bodyheight = $(window).innerHeight();
+        $(".body-content").css("height", bodyheight);
         $("iframe").css("height", (bodyheight *.8)+"px");
     }
 
